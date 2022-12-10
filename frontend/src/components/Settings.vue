437f2934a7e19917b57418d06ae6d4b03cab1dfc
renat <rnagaev68@gmail.com>
2022-12-10 12:21:57 +0300

    checkbox icon

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -2,7 +2,7 @@
   <v-list>
     <v-list-item>
       <v-list-item-action>
-        <v-checkbox v-model="darkTheme"></v-checkbox>
+        <v-checkbox v-model="darkTheme" :on-icon="checkBoxOn" :off-icon="checkBoxOff"></v-checkbox>
       </v-list-item-action>
       <v-list-item-content>
         <v-list-item-title>Темная тема</v-list-item-title>
@@ -14,9 +14,13 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import {mdiCheckboxBlankOutline, mdiCheckboxMarked} from "@mdi/js"
 
 @Component
 export default class Settings extends Vue {
+  checkBoxOff = mdiCheckboxBlankOutline
+  checkBoxOn = mdiCheckboxMarked
+
   get settings() {
     return this.$store.state.settings
   }
